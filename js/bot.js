616f4f603f53f19c2c35d81eaea2c4d6226ca755
Kasper-Loontjens <112395890+Kasper-Loontjens@users.noreply.github.com>
2022-09-23 11:16:32 +0200

    Kommenterat

--- js/bot.js
+++ js/bot.js
@@ -1,4 +1,4 @@
-
+//Gör nav baren synlig eller osynlig beroende på om checkboxen är i klickad.
 function isChecked(){
     if(document.getElementById('som').checked){
         document.getElementById('hand').style.display='flex';
@@ -6,12 +6,13 @@ function isChecked(){
         document.getElementById('hand').style.display='none';
     }
 }
-
+//sparar en image source
 sourcename = document.getElementById('show').src;
-
+//Byter ut image source till en ny
 function displayprojectimg(img){
     document.getElementById('show').src=img;
 }
+//byter tillbaks image source till den sparade
 function stopdisplayprojectimg(){
     document.getElementById('show').src=sourcename;
 }
